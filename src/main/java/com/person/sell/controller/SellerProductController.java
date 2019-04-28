0fc75c3fd9ad38dabe8c1dae57bdc0bd4ed8bc63
YUDI <1019509861@qq.com>
2019-04-28 14:04:15 +0800

    查询商品列表

---
+++ src/main/java/com/person/sell/controller/SellerProductController.java
@@ -0,0 +1,44 @@
+package com.person.sell.controller;
+
+import com.person.sell.dataobject.ProductInfo;
+import com.person.sell.service.ProductInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.xml.ws.Action;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/seller/product")
+public class SellerProductController {
+
+    @Autowired
+    private ProductInfoService productInfoService;
+
+    /**
+     * 查询商品列表
+     * @param page
+     * @param size
+     * @param map
+     * @return
+     */
+    @GetMapping("/list")
+    public ModelAndView list(@RequestParam(value = "page",defaultValue = "1") Integer page,
+                             @RequestParam(value = "size",defaultValue = "10") Integer size,
+                             Map<String, Object> map){
+        PageRequest pageRequest = PageRequest.of(page,size);
+        Page<ProductInfo> productInfoPage = productInfoService.findAll(pageRequest);
+        map.put("productInfoPage",productInfoPage);
+        map.put("currentPage",page);
+        map.put("size",size);
+        return new ModelAndView("product/list",map);
+    }
+
+
+}
