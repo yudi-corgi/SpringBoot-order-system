95119956b0b7eba76ca000783852a90fb62d5d01
YUDI <1019509861@qq.com>
2019-04-30 23:45:11 +0800

    add sellerInfo Service

--- src/main/java/com/person/sell/config/WechatAccountConfig.java
+++ src/main/java/com/person/sell/config/WechatAccountConfig.java
@@ -6,14 +6,23 @@ import org.springframework.stereotype.Component;
 
 /**
  * 微信账号相关配置
+ *
  */
 @Data
 @Component
 @ConfigurationProperties(prefix = "wechat")
 public class WechatAccountConfig {
 
-    private String mpAppId;
-    private String mpAppSecret;
+    private String mpAppId;     //公众平台id
+    private String mpAppSecret; //公众平台密钥
+    /*
+        (微信扫描二维码登录)开放平台暂无相关账号信息，目前只是先配置
+     */
+    private String openAppId;   //开放平台id
+    private String openAppSecret;   //开放平台密钥
+    /*
+        微信支付暂无相关账号信息，目前只是先配置
+     */
     private String mchId;   //商户号
     private String mchKey;  //商户密钥
     private String keyPath; //商户证书路径
