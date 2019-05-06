51320e12cab4fd689b624f1ef40bf5c1978e7dff
YUDI <1019509861@qq.com>
2019-05-06 20:02:08 +0800

    mofidy BuyerProductController.java

--- src/main/java/com/person/sell/controller/BuyerProductController.java
+++ src/main/java/com/person/sell/controller/BuyerProductController.java
@@ -10,6 +10,7 @@ import com.person.sell.service.ProductInfoService;
 import com.person.sell.utils.ResultVOUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.bind.annotation.GetMapping;
